f6f17f58ccea0d2aef638ac897ae7a12ce04d709
maharanawarshweta-max <maharanawarshweta@gmail.com>
2025-08-07 16:35:52 +0530

    Add files via upload

---
+++ FoamDCT_FSB_Realign.bat
@@ -0,0 +1,8 @@
+@echo off 
+
+echo launching First application > C:\temp\testlaunchCatia_BMW.log
+echo Parameter Provided for Launch Script : %1 >> C:\temp\testlaunchNotepad.log
+
+C:\tcplm\FourColorsTechToolbar\BasePackage\Foam_DCT\FoamDCT_FSB\BMW_PACAKAGE\notepad++.exe
+
+echo Notpad launched >> C:\temp\testlaunchCatia_BMW.log
